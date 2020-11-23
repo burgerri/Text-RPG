b4ba88062bd7b5d3585a1c714d48675a9ce3f2a3
Phelino Chua <phelino@gmail.com>
2020-11-23 23:12:28 +1100

    updated login.py

--- login.py
+++ login.py
@@ -1,2 +1,4 @@
+""" This needs to be integrated into the main.py """
+
 name1 = input("Hi, what is the name of your character? ")
 print("Oh hi {}, good to have you join this PvE turn based game".format(name1))
